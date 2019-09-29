e364547a901d5385e6cbc71f0f37e24cf877ec62
Efe <mehmetefeekiner@gmail.com>
2019-09-29 11:02:30 +0200

    fix conflicts

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,1 @@
-<<<<<<< HEAD
-export default () => <p>Welcome to movies!</p>
-=======
-import React from "react";
-
-function Index(props) {
-  return <h1>Hey!!</h1>;
-}
-
-export default Index;
->>>>>>> dfbbbce3599765ab7878cf107b069c7812135b3e
+export default () => <p>Welcome to movies!</p>;
